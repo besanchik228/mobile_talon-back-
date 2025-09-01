f9f8933677a24204e560385102cf906e376b647b
Yegor <yegorsmushkobesan@yandex.ru>
2025-09-01 18:54:28 +0300

    main.py + database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine('sqlite:///./database.db')
 
 session = sessionmaker(bind=engine)
 
-base = declarative_base()
+Base = declarative_base()
 
 def get_db():
     db = session()
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from database import Base, engine
 
 app = FastAPI(title="mobile talon")
 
+origins = ["http://localhost:3000"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_methods=["*"],
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 Base.metadata.create_all(engine)
