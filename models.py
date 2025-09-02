bbd18fb649d03fdd286172a6e71021e837ac07c8
Yegor <yegorsmushkobesan@yandex.ru>
2025-09-02 07:57:48 +0300

    models.py впринципе просто модели юзера и талона

--- models.py
+++ models.py
@@ -6,3 +6,35 @@ import enum
 class UserRole(str, enum.Enum):
     teacher = "teacher"
     canteen = "canteen"
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, index=True)
+    login = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    edu = Column(String, nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
+
+    class_name = Column(String, nullable=True)
+    canteen_id = Column(Integer, ForeignKey('users.id'), nullable=True)
+
+    canteen = relationship("User", remote_side=[id], uselist=False)
+    talons = relationship("Talon", back_populates="teacher")
+
+class Talon(Base):
+    __tablename__ = "talon"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(Date, nullable=False)
+
+    paid_count = Column(Integer, nullable=False)
+    free_count = Column(Integer, nullable=False)
+
+    class_name = Column(String, nullable=False)
+
+    teacher_id = Column(Integer, ForeignKey('users.id'), nullable=True)
+    teacher = relationship("User", back_populates="talons")
+
+    __table_args__ = (
+        UniqueConstraint('teacher_id', 'date', name='uq_talon_teacher_date'),
+    )
